abbc22d7dc2eee5b828ebdf77e45300cd52f0d56
Sean Cross <smcross@gmail.com>
2012-05-18 13:05:02 +0800

    reader: Add partition definition for 20 GB drive

---
+++ reader.c
@@ -0,0 +1,53 @@
+#include <stdint.h>
+
+struct partition {
+    uint8_t *name;
+    uint64_t offset;
+    uint64_t length;
+    uint8_t format;
+};
+
+static struct partition partitions[] = {
+    {
+        .name   = "Security Sector",
+        .offset = 0x2000,
+        .length = 0x40,
+        .format = 0,
+    },
+    {
+        .name   = "System Cache",
+        .offset = 0x80000,
+        .length = 0x80000000,
+        .format = 1,
+    },
+    {
+        .name   = "Game Cache",
+        .offset = 0x80080000,
+        .length = 0xA0E30000,
+        .format = 1,
+    },
+    {
+        .name   = "SysExt",
+        .offset = 0x10C080000,
+        .length = 0xCE30000,
+        .format = 2,
+    },
+    {
+        .name   = "SysExt2",
+        .offset = 0x118EB0000,
+        .length = 0xCE30000,
+        .format = 2,
+    },
+    {
+        .name   = "Xbox 1",
+        .offset = 0x120eb0000,
+        .length = 0x10000000,
+        .format = 2,
+    },
+    {
+        .name   = "Data",
+        .offset = 0x130eb0000,
+        .length = 0,
+        .format = 2,
+    },
+};
