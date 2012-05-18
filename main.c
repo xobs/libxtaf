96ac5c8387a1845a779b2feb20f20c185e7ce842
Sean Cross <smcross@gmail.com>
2012-05-18 14:54:18 +0800

    main: Read out partition #6 (data partition)
    For now, just to make sure things are working, dump out partition #6
    to stdout.  This requires disk and partition code be working properly.

--- main.c
+++ main.c
@@ -2,19 +2,35 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdint.h>
+
+#include "disk.h"
+#include "part.h"
 #include "xtaf.h"
 
 int main (int argc, char **argv) {
-    struct xtaf *xtaf;
+    struct disk *disk;
+    struct part *part;
+    uint64_t i;
+    //struct xtaf *xtaf;
 
     if (argc != 2) {
         printf("Usage: %s [image-file]\n", argv[0]);
         return 1;
     }
 
-    xtaf = xtaf_init(argv[1]);
-    if (!xtaf)
+    disk = disk_init(argv[1]);
+    if (!disk)
         return 1;
+
+
+    part = part_open(disk, 6);
+    if (!part) {
+        perror("Unable to open partition");
+        return 1;
+    }
+
+    for (i=0; i<part_length(part); i++)
+        printf("%c", part_byte(part, i));
 
     return 0;
 }
