7ef704d91a648806a597a5f30ccc29b584caff86
Sean Cross <smcross@gmail.com>
2012-05-21 13:39:09 +0800

    disk: Add disk mmap/munmap routines
    This fixes munmap.

--- disk.c
+++ disk.c
@@ -9,6 +9,8 @@
 struct disk {
     char *filename;
     int fd;
+    uint8_t *mapping;
+    uint64_t mapping_length;
 };
 
 struct disk *disk_init(char *filename) {
@@ -20,6 +22,7 @@ struct disk *disk_init(char *filename) {
 
     disk->filename = strdup(filename);
     disk->fd = open(disk->filename, O_RDONLY);
+    disk->mapping = NULL;
     if (-1 == disk->fd) {
         free(disk);
         return NULL;
@@ -33,16 +36,30 @@ uint64_t disk_length(struct disk *disk) {
     return lseek(disk->fd, 0, SEEK_END);
 }
 
+/* XXX WARNING XXX NON-REENTRANT */
 uint8_t *disk_mmap(struct disk *disk, uint64_t offset, uint64_t length) {
     void *result;
 
+    /* Ensure nothing is mapped */
+    disk_munmap(disk);
+
     /* Allow us to have a "rest-of-the-disk" length */
-    if (!length) {
+    if (!length)
         length = disk_length(disk) - offset;
-    }
 
     result = mmap(NULL, length, PROT_READ, MAP_SHARED, disk->fd, offset);
     if (result == MAP_FAILED)
         return 0;
-    return result;
+
+    disk->mapping = result;
+    disk->mapping_length = length;
+    return disk->mapping;
+}
+
+void disk_munmap(struct disk *disk) {
+    if (!disk->mapping)
+        return;
+    munmap(disk->mapping, disk->mapping_length);
+    disk->mapping = NULL;
+    return;
 }
--- disk.h
+++ disk.h
@@ -5,7 +5,11 @@
 struct disk;
 
 struct disk *disk_init(char *filename);
+
+/* XXX WARNING XXX NON-REENTRANT */
 uint8_t *disk_mmap(struct disk *disk, uint64_t offset, uint64_t length);
+void disk_munmap(struct disk *disk);
+
 uint64_t disk_length(struct disk *disk);
 
 #endif //__DISK_H__
