65dc0a27d28d47f2bb6f73411c9453073b67d705
Sean Cross <smcross@gmail.com>
2012-05-21 13:38:46 +0800

    part: Use enum for part definition

--- part.c
+++ part.c
@@ -1,3 +1,4 @@
+#include <sys/mman.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -7,18 +8,18 @@
 #include "disk.h"
 
 struct partition_def {
-    char     *name;
-    uint64_t offset;
-    uint64_t length;
-    uint8_t  format;
+    char             *name;
+    uint64_t          offset;
+    uint64_t          length;
+    enum part_format  format;
 };
 
 
 struct part {
-    uint32_t id;
+    uint32_t             id;
     struct partition_def *def;
-    uint8_t *data;
-    struct disk *disk;
+    uint8_t              *data;
+    struct disk          *disk;
 };
 
 
@@ -28,48 +29,48 @@ static struct partition_def partitions[] = {
         .name   = "Security Sector",
         .offset = 0x2000,
         .length = 0x40,
-        .format = 0,
+        .format = fmt_none,
     },
     {
         .name   = "System Cache",
         .offset = 0x80000,
         .length = 0x80000000,
-        .format = 1,
+        .format = fmt_sfcx,
     },
     {
         .name   = "Game Cache",
         .offset = 0x80080000,
         .length = 0xA0E30000,
-        .format = 1,
+        .format = fmt_sfcx,
     },
     {
         .name   = "SysExt",
         .offset = 0x10C080000,
         .length = 0xCE30000,
-        .format = 2,
+        .format = fmt_xtaf,
     },
     {
         .name   = "SysExt2",
         .offset = 0x118EB0000,
         .length = 0xCE30000,
-        .format = 2,
+        .format = fmt_xtaf,
     },
     {
         .name   = "Xbox 1",
         .offset = 0x120eb0000,
         .length = 0x10000000,
-        .format = 2,
+        .format = fmt_xtaf,
     },
     {
         .name   = "Data",
         .offset = 0x130eb0000,
         .length = 0,
-        .format = 2,
+        .format = fmt_xtaf,
     },
 };
 
 
-struct part *part_open(struct disk *disk, int part_id) {
+struct part *part_init(struct disk *disk, int part_id) {
     struct part *part;
 
     part = malloc(sizeof(struct part));
@@ -86,7 +87,8 @@ struct part *part_open(struct disk *disk, int part_id) {
     part->def = &partitions[part->id];
     part->disk = disk;
 
-    fprintf(stderr, "Opening partition %s...\n", part->def->name);
+    fprintf(stderr, "Opening partition %s offset 0x%llx...\n",
+            part->def->name, part->def->offset);
     if (!part->def->length)
         part->def->length = disk_length(part->disk) - part->def->offset;
 
@@ -100,6 +102,16 @@ struct part *part_open(struct disk *disk, int part_id) {
     return part;
 }
 
+void part_free(struct part **part) {
+    if (!*part)
+        return;
+    disk_munmap((*part)->disk);
+
+    free(*part);
+    *part = NULL;
+    return;
+}
+
 uint64_t part_length(struct part *part) {
     return part->def->length;
 }
@@ -110,4 +122,8 @@ uint8_t part_byte(struct part *part, uint64_t offset) {
 
 const uint8_t *part_data(struct part *part) {
     return part->data;
+}
+
+enum part_format part_format(struct part *part) {
+    return part->def->format;
 }
--- part.h
+++ part.h
@@ -1,13 +1,20 @@
 #ifndef __PART_H__
 #define __PART_H__
 
+enum part_format {
+    fmt_none,   // Unformatted (e.g. security sector)
+    fmt_sfcx,   // Secure File Cache for Xbox
+    fmt_xtaf,   // Little-endian FATX
+};
 struct disk;
 struct part;
 
-struct part *part_open(struct disk *disk, int part_id);
+struct part *part_init(struct disk *disk, int part_id);
+void part_free(struct part **part);
 
 uint64_t part_length(struct part *part);
 const uint8_t *part_data(struct part *part);
 uint8_t part_byte(struct part *part, uint64_t offset);
+enum part_format part_format(struct part *part);
 
 #endif //__PART_H__
